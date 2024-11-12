338a014c0f7aa4d9eee93b058cc3abd6b6c5223a
엄시윤 <happiness010108@gmail.com>
2024-11-12 21:06:04 +0700

    [엄시윤] - mybatis / insert, update, delete 연습

--- src/main/java/com/mikeyyun/spring/test/mybatis/RealEstateController.java
+++ src/main/java/com/mikeyyun/spring/test/mybatis/RealEstateController.java
@@ -2,8 +2,10 @@ package com.mikeyyun.spring.test.mybatis;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +15,7 @@ import com.mikeyyun.spring.test.mybatis.domain.RealEstate;
 import com.mikeyyun.spring.test.mybatis.service.RealEstateService;
 
 
-@RequestMapping("/mybatis/realestate/select")
+@RequestMapping("/mybatis/realestate")
 @Controller
 public class RealEstateController {
 	
@@ -21,7 +23,7 @@ public class RealEstateController {
 	private RealEstateService realEstateService;
 	
 	@ResponseBody
-	@RequestMapping("/1")
+	@RequestMapping("select/1")
 	public RealEstate realEstate(@RequestParam("id")int id) {
 		
 		// parameter 로 전달 받은 id 와 일치하는 부동산 정보 얻어오기
@@ -31,14 +33,14 @@ public class RealEstateController {
 	}
 	
 	@ResponseBody
-	@RequestMapping("/2")
+	@RequestMapping("select/2")
 	public List<RealEstate> RealEstatePrice(@RequestParam("price") int price) {
 		List<RealEstate> realEstatePrice = realEstateService.getRealEstatePrice(price);
 		return realEstatePrice;
 	}
 	
 	@ResponseBody
-	@RequestMapping("/3")
+	@RequestMapping("select/3")
 	public List<RealEstate> realEstateByAreaAndPrice(
 			@RequestParam("area") int area
 			, @RequestParam("price") int price) {
@@ -46,6 +48,59 @@ public class RealEstateController {
 		List<RealEstate> realEstateByAreaAndPrice = realEstateService.getRealEstateByAreaAndPrice(area, price);
 		
 		return realEstateByAreaAndPrice;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/insert/1")
+	public String createRealEstateByObject() {
+//		realtorId : 3
+//		address : 푸르지용 리버 303동 1104호
+//		area : 89
+//		type : 매매
+//		price : 100000
+		
+		RealEstate realEstate = new RealEstate();
+		realEstate.setRealtorId(3);
+		realEstate.setAddress("한남동 유엔빌리지 파트코인빌 201호");
+		realEstate.setArea(89);
+		realEstate.setType("매매");
+		realEstate.setPrice(670000);
+		
+		int count = realEstateService.addRealEstateByObject(realEstate);
+		
+		return "입력 성공 : " + count;
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping("/insert/2")
+	public String creatRealEstate(@RequestParam("realtorId") int realtorId) {
+		
+//		address : 썅떼빌리버 오피스텔 814호
+//		area : 45
+//		type : 월세
+//		price : 100000
+//		rentPrice : 120
+		
+		int count = realEstateService.addRealEstate(realtorId, "NYC H", 100, "월세", 10000000, 10000);
+		
+		return "입력 성공 : " + count;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update")
+	public String updateRealEstate() {
+		int count = realEstateService.updateRealEstate(2, "전세", 70000);
+		
+		return "수정 성공 : " + count;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/delete")
+	public String deleteRealEstate(@RequestParam("id") int id) {
+		int count = realEstateService.deleteRealEstate(id);
+		
+		return "삭제 성공 : " + count;
 	}
 
 }
--- src/main/java/com/mikeyyun/spring/test/mybatis/service/RealEstateService.java
+++ src/main/java/com/mikeyyun/spring/test/mybatis/service/RealEstateService.java
@@ -12,25 +12,60 @@ import com.mikeyyun.spring.test.mybatis.repository.RealEstateRepository;
 public class RealEstateService {
 	
 	@Autowired
-	private RealEstateRepository realEstrateRepository;
+	private RealEstateRepository realEstateRepository;
 	
 	public RealEstate getRealEstate(int id) {
 		
-		RealEstate realEstate = realEstrateRepository.selectRealEstate(id);
+		RealEstate realEstate = realEstateRepository.selectRealEstate(id);
 		
 		return realEstate;
 	}
 	
 	public List<RealEstate> getRealEstatePrice(int price) {
 		
-		List <RealEstate> realEstatePrice = realEstrateRepository.selectRealEstatePrice(price);
+		List <RealEstate> realEstatePrice = realEstateRepository.selectRealEstatePrice(price);
 		
 		return realEstatePrice;
 	}
 
 	public List<RealEstate> getRealEstateByAreaAndPrice(int area, int price) {
-		List<RealEstate> realEstateByAreaAndPrice = realEstrateRepository.selectRealEstatebyAreaAndPrice(area, price);
+		List<RealEstate> realEstateByAreaAndPrice = realEstateRepository.selectRealEstatebyAreaAndPrice(area, price);
 		
 		return realEstateByAreaAndPrice;
 	}
+	
+	public int addRealEstateByObject(RealEstate realEstate) {
+		int count = realEstateRepository.insertRealEstateByObject(realEstate);
+		
+		return count;
+	}
+	
+	public int addRealEstate(
+			int realtorId
+			, String address
+			, int area
+			, String type
+			, int price
+			, int rentPrice) {
+		
+		int count = realEstateRepository.insertRealEstate(realtorId, address, area, type, price, rentPrice);
+		
+		return count;
+	}
+	
+	public int updateRealEstate(
+			  int id
+			, String type
+			, int price) {
+		int count = realEstateRepository.updateRealEstate(id, type, price);
+		
+		return count;
+	}
+	
+	public int deleteRealEstate(int id) {
+		int count = realEstateRepository.deleteRealEstate(id);
+		
+		return count;
+	}
+	
 }
