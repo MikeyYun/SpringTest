02cda9b733088fd3eb7ceab8f471ce4bb5a6c970
엄시윤 <happiness010108@gmail.com>
2024-11-14 09:20:33 +0700

    [엄시윤] - mvc cycle 조회된 결과를 hdml을 통해 timelife라는 문법을 활용해서 html로 response로 구성하는 방법에 대한 예제

---
+++ src/main/java/com/mikeyyun/spring/test/mvc/service/SellerService.java
@@ -0,0 +1,37 @@
+package com.mikeyyun.spring.test.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mikeyyun.spring.test.mvc.domain.Seller;
+import com.mikeyyun.spring.test.mvc.repository.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	public int addSeller(
+			String nickname
+			, String profileImage
+			, double temperature) {
+		
+			int count = sellerRepository.insertSeller(nickname, profileImage, temperature);
+		
+			return count;
+	}
+	public Seller getLastSeller() {
+			Seller seller = sellerRepository.selectLastSeller();
+	
+			return seller;
+	}
+
+	public Seller getSeller(int id) {
+			Seller seller = sellerRepository.selectSeller(id);
+		
+			return seller;
+	
+	}
+	
+}
