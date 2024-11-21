7126bb51f483cac8bd04737e5e6d241881d28e75
엄시윤 <happiness010108@gmail.com>
2024-11-21 16:47:45 +0700

    [엄시윤] - ajax 중복확인

---
+++ src/main/java/com/mikeyyun/spring/test/ajax/repository/FavoriteRepository.java
@@ -0,0 +1,20 @@
+package com.mikeyyun.spring.test.ajax.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.mikeyyun.spring.test.ajax.domain.Favorite;
+
+@Mapper
+public interface FavoriteRepository {
+	
+	public List<Favorite> selectFavoriteList();
+	
+	public int insertFavorite(
+			@Param("name") String name
+			, @Param("url") String url);
+	
+	public int countByUrl(@Param("url") String url);
+}
---
+++ src/main/java/com/mikeyyun/spring/test/ajax/service/FavoriteService.java
@@ -0,0 +1,41 @@
+package com.mikeyyun.spring.test.ajax.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mikeyyun.spring.test.ajax.domain.Favorite;
+import com.mikeyyun.spring.test.ajax.repository.FavoriteRepository;
+
+@Service
+public class FavoriteService {
+	
+	@Autowired
+	private FavoriteRepository favoriteRepository;
+	
+	public List<Favorite> getFavoriteList() {
+		
+		List<Favorite> favoriteList = favoriteRepository.selectFavoriteList();
+		
+		return favoriteList;
+	}
+	
+	public int addFavorite(String name, String url) {
+		int count = favoriteRepository.insertFavorite(name, url);
+		return count;
+	}
+	
+	public boolean isDuplicateUrl(String url) {
+		
+		int count = favoriteRepository.countByUrl(url);
+		
+		return count > 0;
+		
+//		if(count > 0) {
+//			return true;
+//		} else {
+//			return false;
+//		}
+	}
+}
